4a402c51fce2ec696da00561977151db057736b0
Khushi Kumawat <lilakumawat30@gmail.com>
2025-10-29 14:14:26 +0530

    randomDotReact

--- dot/src/App.jsx
+++ dot/src/App.jsx
@@ -39,9 +39,9 @@ console.log(dotobj)
 
   return (
     <div onClick={handleClick} className="randomdot">
-      <button onClick={resetBtn}>Reset</button>
-      <button onClick={undoBtn}>Undo</button>
-      <button onClick={redoBtn}>Redo</button>
+      <button onClick={resetBtn} disabled={circles.length===0}>Reset</button>
+      <button onClick={undoBtn}disabled={circles.length===0}>Undo</button>
+      <button onClick={redoBtn} disabled={deleteCircle.length===0}>Redo</button>
 
       <div>
         {circles.map((circle, index) => (
