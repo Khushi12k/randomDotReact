7b4314bac01c99fb5a4d10288da123efba45180b
Khushi Kumawat <lilakumawat30@gmail.com>
2025-10-29 14:03:16 +0530

    randomDotReact

---
+++ dot/src/App.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [circles, setCircles] = useState([]);
+  const [deleteCircle, setDeleteCircle] = useState([]);
+
+  const colours = ["red", "blue", "green", "yellow", "indigo", "violet"];
+
+  function handleClick(e) {
+    if (e.target.tagName === "BUTTON") return;
+
+    const dotobj = {
+      color: colours[Math.floor(Math.random() * colours.length)],
+      id: Date.now(),
+      x: e.clientX,
+      y: e.clientY,
+    };
+console.log(dotobj)
+    setCircles([...circles, dotobj]);
+  }
+
+  function resetBtn() {
+    setCircles([]);
+    setDeleteCircle([]);
+  }
+
+  function undoBtn(){
+    if(circles.length===0) return
+    let print=circles
+    let insert=print.pop()
+    setDeleteCircle([...deleteCircle,insert])
+  }
+  function redoBtn(){
+    if(deleteCircle.length===0)return
+    let print=deleteCircle
+    let last=print.pop()
+    setCircles([...circles,last])
+  }
+
+  return (
+    <div onClick={handleClick} className="randomdot">
+      <button onClick={resetBtn}>Reset</button>
+      <button onClick={undoBtn}>Undo</button>
+      <button onClick={redoBtn}>Redo</button>
+
+      <div>
+        {circles.map((circle, index) => (
+          <div
+            key={circle.id}
+            className="circle"
+            style={{
+              top: circle.y - 7.5 + "px",
+              left: circle.x - 7.5 + "px",
+              backgroundColor: circle.color,
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
